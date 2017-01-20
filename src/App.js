7b8e6abe09afc63702c47988ccd5f50958ee06ea
Joe Spedale <jspedale@gmail.com>
2017-01-19 23:53:59 -0500

    Used promises to query FCC JSON API and populate state with users.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Leaderboard from './Leaderboard';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users30: {},
+      usersAllTime: {}
+    }
+  }
+
+  componentWillMount() {
+    const topMonthPromise = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+    topMonthPromise
+      .then(data => data.json())
+      .then(data => this.setState({users30: data}))
+      .catch(err => console.error(err));
+
+    const allTimePromise = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+    allTimePromise
+      .then(data => data.json())
+      .then(data => this.setState({usersAllTime: data}))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>freeCodeCamp Leaderboard</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          view all-time
         </p>
+        <Leaderboard
+          topMonth={this.state.users30}
+          topAllTime={this.state.usersAllTime}
+        />
       </div>
     );
   }
---
+++ src/Leaderboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Leaderboard extends Component {
+  render() {
+    return (
+    <div>this is where the leaderboard goes</div>
+    )
+  }
+
+}
+
+
+
+
+
+export default Leaderboard
